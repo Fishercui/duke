df8321add8c7e085e8b6fefce9daf9b31ec777f9
Fisher Cui <cuijordan388@hotmail.com>
2021-11-14 10:49:45 +0800

    Delete feature

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -4,7 +4,8 @@ public class DeleteCommand extends Command {
 
     public static final String COMMAND_WORD="delete";
 
-    public static final String MESSAGE_USAGE="||"+COMMAND_WORD+": delete task from the tasklist based on the task index.\n";
+    public static final String HELP_MESSAGE="-- "+COMMAND_WORD+": delete task from the tasklist based on the task index.\n" +
+            "<< Example: delete 1 >>";
 
 
     public DeleteCommand(int targetIndex) {
