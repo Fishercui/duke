dca708531897d3a965426dc802975b49d32c9632
Fisher Cui <cuijordan388@hotmail.com>
2021-11-14 10:51:34 +0800

    Task type icon resolved

--- src/main/java/task/ToDo.java
+++ src/main/java/task/ToDo.java
@@ -3,29 +3,28 @@ package task;
 import java.time.LocalDateTime;
 
 public class ToDo extends Task {
+    private static final String TASK_TYPE = "T";
 
     public ToDo(String content) {
         super(content);
     }
 
     public ToDo(String description, boolean isDone, LocalDateTime finishTime) {
-        super(description,isDone,finishTime);
+        super(description, isDone, finishTime);
     }
 
-    public ToDo editContent (String newContent) {
+    public ToDo editContent(String newContent) {
         ToDo newTask = new ToDo(newContent);
         newTask.isDone = this.isDone;
         return newTask;
     }
 
-
+    public String getTaskType() {
+        return TASK_TYPE;
+    }
 
     @Override
     public String toString() {
-        if (!this.isDone) {
-            return "T | 0 | " + super.toString();
-        } else {
-            return "T | 1 | " + super.toString();
-        }
+        return "[T]" + super.toString();
     }
 }
